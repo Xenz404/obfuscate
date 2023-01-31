6710976a194c3906060a85b40e212906ef15fcf9
/Xenz404 <116076493+Xenz404@users.noreply.github.com>
2023-01-31 19:02:56 +0700

    Update Run.py

--- Run.py
+++ Run.py
@@ -2,8 +2,8 @@
 #Mau recode ya_-
 import os
 if __name__ == "__main__":
-        try:
-                os.system("git pull")
-                __import__("__xenz__obfuscate__").Main()
-        except Exception as e:
-                exit(str(e))
+    try:
+        os.system("git pull")
+        __import__("xenz").Main()
+    except Exception as e:
+        exit(str(e))
