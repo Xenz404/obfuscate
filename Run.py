62b19aec434532f4514273ad5cd314ccb8a767a1
/Xenz404 <116076493+Xenz404@users.noreply.github.com>
2023-01-31 18:58:29 +0700

    Update Run.py

--- Run.py
+++ Run.py
@@ -1,9 +1,9 @@
-#Hai bang mau ngpain
+2#Hai bang mau ngpain
 #Mau recode ya_-
 import os
 if __name__ == "__main__":
         try:
                 os.system("git pull")
-                __import__("__xenz__obfuscate__").main()
+                __import__("__xenz__obfuscate__").Main()
         except Exception as e:
                 exit(str(e))
