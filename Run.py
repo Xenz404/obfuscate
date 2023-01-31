1ad82e83e9733506d91631906b69b730936813c3
/Xenz404 <116076493+Xenz404@users.noreply.github.com>
2023-01-31 19:10:20 +0700

    Delete Run.py

--- Run.py
+++
@@ -1,9 +0,0 @@
-#Hai bang mau ngpain
-#Mau recode ya_-
-import os
-if __name__=="__main__":
-    try:
-        os.system("git pull")
-        __import__("xenz").Main()
-    except Exception as e:
-        exit(str(e))
