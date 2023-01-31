0b95354dc32c3774ffc8a49f7c89718c634e13b3
/Xenz404 <116076493+Xenz404@users.noreply.github.com>
2023-01-31 19:04:54 +0700

    Update Run.py

--- Run.py
+++ Run.py
@@ -1,7 +1,7 @@
-2#Hai bang mau ngpain
+#Hai bang mau ngpain
 #Mau recode ya_-
 import os
-if __name__ == "__main__":
+if __name__=="__main__":
     try:
         os.system("git pull")
         __import__("xenz").Main()
